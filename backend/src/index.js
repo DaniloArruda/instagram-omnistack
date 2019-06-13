3d061227c843b42f5c95b236e2bc1aae8b9b079e
DaniloArruda <da.ni.lo_arruda@outlook.com>
2019-06-13 19:21:41 -0300

    corrigindo index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-const server = require('http').Server();
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect("mongodb+srv://danilo:cuia1234@pagamento-cluster-vhk3r.mongodb.net/test?retryWrites=true",
