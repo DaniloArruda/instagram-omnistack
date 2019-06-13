1781b5c4f9fa0e3972c5ca08a6ca3420f51a527a
DaniloArruda <da.ni.lo_arruda@outlook.com>
2019-06-13 17:41:08 -0300

    commit inicial com backend criado

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server();
+const io = require('socket.io')(server);
+
+mongoose.connect("mongodb+srv://danilo:cuia1234@pagamento-cluster-vhk3r.mongodb.net/test?retryWrites=true",
+  { useNewUrlParser: true })
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+})
+app.use(cors());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+app.use(require('./routes'));
+
+server.listen(3333);
